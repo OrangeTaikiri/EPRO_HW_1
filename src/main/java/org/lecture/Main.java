150894aae672147f82ae83f6e4eb84f4d836a725
orangetaikiri <rafche@gmail.com>
2025-09-27 21:59:56 +0200

    fixes

--- src/main/java/org/lecture/Main.java
+++ src/main/java/org/lecture/Main.java
@@ -4,24 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    static void main() {
+    public static void main(String[] args) {
 
-        //scanner for reading input value
         Scanner scanner = new Scanner(System.in);
 
         char option;
-        //do-while for looping at least once
+
         do {
-
-            float result = 0.0F;
-
-            // using triple quotes to write more than one line
             System.out.println("""
-                    Wählen sie folgende Umrechnungs-Optionen aus:
+                    Wählen Sie folgende Umrechnungs-Optionen aus:
                     a -> Kilometer in Meilen umrechnen
                     b -> Kilogramm in Pfund umrechnen
                     c -> Celsius in Fahrenheit
-                    d -> eigene Umrechnung druchführen 
+                    d -> eigene Umrechnung durchführen
                     x -> Programm beenden
                     """);
 
@@ -31,44 +26,31 @@ public class Main {
                 break;
             }
 
-
-            System.out.println("Bitte geben sie die zu konvertierente Menge an");
-
-            // input of the conversion Value
+            System.out.println("Bitte geben Sie die zu konvertierende Menge an:");
             float value = scanner.nextFloat();
-            scanner.nextLine();
 
-            // doing the conversion km to mi
             if (option == 'a') {
-                System.out.printf("%.2f km ergeben %.2f Meilen\n", value, value * 0.621371F);
-            }
-
-            // doing the conversion Kilo to Pound
-            if (option == 'b') {
-                System.out.printf("%.2f Kilo ergeben %.2f Pfund\n", value, value * 2.2046F);
-            }
-
-            // doing the conversion celsius to fahrenheit
-            if (option == 'c') {
-                System.out.printf("%.2f Celsius Grad ergeben %.2f Fahrenheit\n", value, (value * 9 / 5) + 32);
-            }
-
-            // doing the custom conversion
-            if (option == 'd') {
-                System.out.println("Geben sie Ausgangseinheit an");
+                System.out.printf("%.2f km ergeben %.2f Meilen%n", value, value * 0.621371F);
+            } else if (option == 'b') {
+                System.out.printf("%.2f Kilo ergeben %.2f Pfund%n", value, value * 2.2046F);
+            } else if (option == 'c') {
+                System.out.printf("%.2f Celsius Grad ergeben %.2f Fahrenheit%n", value, (value * 9.0F / 5.0F) + 32);
+            } else if (option == 'd') {
+                System.out.println("Geben Sie Ausgangseinheit an:");
                 String startUnit = scanner.next();
-                System.out.println("Geben sie Zieleinheit an");
+                System.out.println("Geben Sie Zieleinheit an:");
                 String targetUnit = scanner.next();
                 System.out.println("Bitte geben Sie den gewünschten Umrechnungsfaktor ein:");
                 float factor = scanner.nextFloat();
-                System.out.printf("%.2f %s ergeben %.2f %s ", value, startUnit,  value * factor, targetUnit);
+                System.out.printf("%.2f %s ergeben %.2f %s%n", value, startUnit, value * factor, targetUnit);
             }
 
             System.out.println("Möchten Sie noch eine Einheit umrechnen? nein -> x");
             option = scanner.next().charAt(0);
 
-        }
-        while (option != 'x');
-    }
+        } while (option != 'x');
 
+        scanner.close();
+    }
 }
+
