a9d85e4d52adf5a89b9d5e2da83c7c4fb5bfdd4f
orangetaikiri <Rafche@gmail.com>
2025-10-03 14:14:25 +0200

    convert to switch/case

--- src/main/java/org/lecture/Main.java
+++ src/main/java/org/lecture/Main.java
@@ -30,20 +30,20 @@ public class Main {
             System.out.println("Bitte geben Sie die zu konvertierende Menge an:");
             float value = scanner.nextFloat();
 
-            if (option == 'a') {
-                System.out.printf("%.2f km ergeben %.2f Meilen%n", value, value * 0.621371F);
-            } else if (option == 'b') {
-                System.out.printf("%.2f Kilo ergeben %.2f Pfund%n", value, value * 2.2046F);
-            } else if (option == 'c') {
-                System.out.printf("%.2f Celsius Grad ergeben %.2f Fahrenheit%n", value, (value * 9.0F / 5.0F) + 32);
-            } else if (option == 'd') {
-                System.out.println("Geben Sie Ausgangseinheit an:");
-                String startUnit = scanner.next();
-                System.out.println("Geben Sie Zieleinheit an:");
-                String targetUnit = scanner.next();
-                System.out.println("Bitte geben Sie den gewünschten Umrechnungsfaktor ein:");
-                float factor = scanner.nextFloat();
-                System.out.printf("%.2f %s ergeben %.2f %s%n", value, startUnit, value * factor, targetUnit);
+            switch (option) {
+                case 'a' -> System.out.printf("%.2f km ergeben %.2f Meilen%n", value, value * 0.621371F);
+                case 'b' -> System.out.printf("%.2f Kilo ergeben %.2f Pfund%n", value, value * 2.2046F);
+                case 'c' ->
+                        System.out.printf("%.2f Celsius Grad ergeben %.2f Fahrenheit%n", value, (value * 9.0F / 5.0F) + 32);
+                case 'd' -> {
+                    System.out.println("Geben Sie Ausgangseinheit an:");
+                    String startUnit = scanner.next();
+                    System.out.println("Geben Sie Zieleinheit an:");
+                    String targetUnit = scanner.next();
+                    System.out.println("Bitte geben Sie den gewünschten Umrechnungsfaktor ein:");
+                    float factor = scanner.nextFloat();
+                    System.out.printf("%.2f %s ergeben %.2f %s%n", value, startUnit, value * factor, targetUnit);
+                }
             }
 
             System.out.println("Möchten Sie noch eine Einheit umrechnen? nein -> x");
